9fe1e553f3c38b7f1a139edf9a0a84983879f1b3
Alexander <lotos_toi@mail.ru>
2019-10-10 21:45:41 +1000

    пости готово

--- src/js/lesson3-task3.js
+++ src/js/lesson3-task3.js
@@ -11,7 +11,9 @@ let obj = {
         max = 4;
         rndV = Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
         for (let i = 0; i < this.vall.length; i++) {
-            if ((i + 1) == rndV) { return this.vall[i] }
+            if ((i + 1) == rndV) {
+                return this.vall[i]
+            }
 
         }
     }
@@ -26,37 +28,74 @@ let obj = {
 function funClikB() {
     you = obj.vall[0]
     computer = obj.rndVal()
-    console.log(you + " rrr " + computer)
-    if (you === computer) {
-        d.getElementById('left').setAttribute("background", "url(./../../img/hand.png)")
-        d.getElementById('left').setAttribute("background", "url(./../../img/kyluck.png)")
+    if ((you === computer) && (you === obj.vall[0])) {
+        d.getElementById('left').className = 'windGame__h'
+        d.getElementById('right').className = 'windGame__h'
 
+        d.getElementById('itog').innerHTML = "Ничья!"
     }
 
     if ((you === "бумага") && (computer === "камень")) {
-        d.getElementById('left').setAttribute("background-image", "./../img/hand.png")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png")
+        d.getElementById('left').className = 'windGame__h'
+        d.getElementById('right').className = 'windGame__k'
+        d.getElementById('itog').innerHTML = "Ты выиграл"
+
     }
     if ((you === "бумага") && (computer === "ножници")) {
-    d.getElementById('left').setAttribute("background-image", "./../img/hand.png")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png)")
-    }
-    if ((you === "камень") && (computer === "ножници")) {
-    d.getElementById('left').setAttribute("background-image", "./../img/hand.png)")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png)")
-    }
-    if ((you === "камень") && (computer === "бумага")) {
-    d.getElementById('left').setAttribute("background-image", "./../img/hand.png)")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png)")
-    }
-    if ((you === "ножници") && (computer === "бумага")) {
-    d.getElementById('left').setAttribute("background-image", "./../img/hand.png)")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png)")
-    }
-    if ((you === "ножници") && (computer === "камень")) {
-    d.getElementById('left').setAttribute("background-image", "./../img/hand.png)")
-        d.getElementById('left').setAttribute("background-image", "./../img/kyluck.png)")
+        d.getElementById('left').className = 'windGame__h'
+        d.getElementById('right').className = 'windGame__k'
+        d.getElementById('itog').innerHTML = "Ты проиграл"
+
+
     }
 }
 
+function funClikK() {
+    console.log(111)
+    you = obj.vall[1]
+    computer = obj.rndVal()
 
+    if ((you === computer) && (you === obj.vall[1])) {
+        d.getElementById('left').className = 'windGame__k'
+        d.getElementById('right').className = 'windGame__k'
+        d.getElementById('itog').innerHTML = "Ничья!"
+    }
+
+    if ((you === "камень") && (computer === "ножници")) {
+        d.getElementById('left').className = 'windGame__k'
+        d.getElementById('right').className = 'windGame__n'
+        d.getElementById('itog').innerHTML = "Ты выиграл"
+
+    }
+    if ((you === "камень") && (computer === "бумага")) {
+        d.getElementById('left').className = 'windGame__k'
+        d.getElementById('right').className = 'windGame__h'
+        d.getElementById('itog').innerHTML = "Ты проиграл"
+
+    }
+
+}
+
+function funClikN() {
+
+    you = obj.vall[2]
+    computer = obj.rndVal()
+
+    if ((you === computer) && (you === obj.vall[2])) {
+        d.getElementById('left').className = 'windGame__n'
+        d.getElementById('right').className = 'windGame__n'
+        d.getElementById('itog').innerHTML = "Ничья!"
+    }
+
+    if ((you === "ножници") && (computer === "бумага")) {
+        d.getElementById('left').className = 'windGame__n'
+        d.getElementById('right').className = 'windGame__h'
+        d.getElementById('itog').innerHTML = "Ты выиграл"
+
+    }
+    if ((you === "ножници") && (computer === "камень")) {
+        d.getElementById('left').className = 'windGame__n'
+        d.getElementById('right').className = 'windGame__k'
+        d.getElementById('itog').innerHTML = "Ты проиграл"
+    }
+}

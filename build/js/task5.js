80cff9a2a3b85bbb14f0474ab00573e1c781cbbd
lotostoii <lotostoii@gmail.com>
2019-10-15 16:37:55 +1000

    калькулятор готов

--- build/js/task5.js
+++ build/js/task5.js
@@ -3,24 +3,24 @@ function calc() {
 
     contDinPage.className = 'calc'
 
-    let arrButtons = [
-        ['number', 'number', '0' , '0'],
-        ['number', 'number', '1' , '1'],
-        ['number', 'number', '2' , '2'],
-        ['number', 'number', '3' , '3'],
-        ['number', 'number', '4' , '4'],
-        ['number', 'number', '5' , '5'],
-        ['number', 'number', '6' , '6'],
-        ['number', 'number', '7' , '7'],
-        ['number', 'number', '8' , '8'],
-        ['number', 'number', '9' , '9'],
-        ['point', 'point', 'point' , '.'],
-        ['opereation', 'opereation', '-' , '-'],
-        ['opereation', 'opereation', '+' , '+'],
-        ['opereation', 'opereation', '*' , '*'],
-        ['opereation', 'opereation', '/' , '/'],
-        ['opereation', 'opereation', '=' , '='],
-        ['opereation', 'opereation', 'clean' , 'C'],
+    let arrButtons = [ //массив кнопок калькулятора [css-class] [data-х] [value data-x] [ value of tegs]
+        ['number', 'number', '0', '0'],
+        ['number', 'number', '1', '1'],
+        ['number', 'number', '2', '2'],
+        ['number', 'number', '3', '3'],
+        ['number', 'number', '4', '4'],
+        ['number', 'number', '5', '5'],
+        ['number', 'number', '6', '6'],
+        ['number', 'number', '7', '7'],
+        ['number', 'number', '8', '8'],
+        ['number', 'number', '9', '9'],
+        ['point', 'number', '.', '.'],
+        ['opereation', 'opereation', '-', '-'],
+        ['opereation', 'opereation', '+', '+'],
+        ['opereation', 'opereation', '*', '*'],
+        ['opereation', 'opereation', '/', '/'],
+        ['opereation', 'opereation', '=', '='],
+        ['opereation', 'opereation', 'clean', 'C'],
     ]
 
 
@@ -29,9 +29,57 @@ function calc() {
     <div class="wind"></div>
     </div>
     `
-    arrButtons.forEach((el,i) => {
-       d.querySelector('.bodyCalc').innerHTML += `<button class="${el[0]}" data-${el[1]}="${el[2]}" >${el[3]}</button>`
+    let var1 = ''
+    let opereation = ''
+    let opereationReady = false 
+
+    let bodyCalc = d.querySelector('.bodyCalc')
+    var win = d.querySelector('.wind')
+    win.innerHTML = win.innerHTML = ''
+
+
+    arrButtons.forEach((el, i) => {
+        bodyCalc.innerHTML += `<button class="${el[0]}" data-${el[1]}="${el[2]}" >${el[3]}</button>`
     })
+
+
+
+    bodyCalc.addEventListener('click', collectNumber)
+
+
+
+    function collectNumber(event) {
+        win = d.querySelector('.wind')
+        if (event.target.dataset['number']) { //собираем число!
+            if (opereationReady) {win.innerHTML=''}
+            win.innerHTML += event.target.innerHTML
+            opereationReady=false 
+        }
+        if (event.target.dataset['opereation'] == 'clean') {
+            win.innerHTML = ''
+        }
+        if ((event.target.dataset['opereation'] == '-')||(event.target.dataset['opereation'] == '+')||(event.target.dataset['opereation'] == '*')||(event.target.dataset['opereation'] == '/')) {
+            var1 = +win.innerHTML
+            opereation = event.target.dataset['opereation']
+            win.innerHTML = ''
+              console.log(event.target.dataset['opereation'])
+        }
+        if (event.target.dataset['opereation'] == '=') {
+            switch (opereation) {
+                case '-': win.innerHTML = var1 - (+win.innerHTML)
+                    break
+                case '+': win.innerHTML = var1 + (+win.innerHTML)
+                    break
+                case '*': win.innerHTML = var1 * (+win.innerHTML)
+                    break
+                case '/': ((+win.innerHTML) == 0 ) ? win.innerHTML = "Деление на 0!"  :  win.innerHTML = var1 / (+win.innerHTML)         
+                    break
+                default: win.innerHTML = ''
+            }
+            opereationReady=true
+        }
+    }
+
 
 
 
